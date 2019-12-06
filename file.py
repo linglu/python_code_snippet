dc6289a5966b89898e7aa43549be5123f7dc4204
刘雄斌 <2855140298@qq.com>
2019-12-06 11:32:59 +0800

    添加文件过滤方法

--- file.py
+++ file.py
@@ -11,3 +11,11 @@ def print_every_dir_path(dirs):
     for path, dirnames, filenames in os.walk(dirs):
         for d in dirnames:
             print(path + "/" + d)
+
+# 过滤文件
+def filter_files_with():
+    for path, dirnames, filenames in os.walk("."):
+        for f in filenames:
+            if type in f:
+                fs = path "/" + f
+                yield fs
